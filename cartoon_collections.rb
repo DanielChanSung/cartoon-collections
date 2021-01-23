b3f0c08a327c75370cebe1fd096e8a5d6531e647
Daniel Sung <DanielChanSung@gmail.com>
2021-01-22 20:12:16 -0500

    commit tree 87a95d024f4adae51672d5fa8267bc6f9e2e5e3f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+dwarves = ["Doc" "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
+    i = 0
+    dwarves.each_with_index { |item, index| puts "#{index + 1}:#{item}" }
+    i = i + 1    
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  # code an argument here
+    i = 0
+    array = []
+    planeteer_calls.map { |string| string.capitalize + "!" }
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+assorted_words = ["two", "go", "industrious", "bop"]
+short_words = ["puff", "go", "two"]
+
+def long_planeteer_calls(assorted_word)# code an argument here
+  assorted_word.any? { |word| word.length >4 }
+     
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find{ | yellowstuff | cheese_types.include?(yellowstuff) }
 end
